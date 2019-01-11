27d45afe1275396f540d8294f1fd02509bfd7a89
Sanat Mharolkar <Sanat Mharolkar>
2019-01-11 23:50:07 +0530

    robot_leg loop fix

--- RoboCon2019/TestCodes/Quadruped/ROS-Ak/robot_leg/scripts/leg_control.py
+++ RoboCon2019/TestCodes/Quadruped/ROS-Ak/robot_leg/scripts/leg_control.py
@@ -14,31 +14,21 @@ def my_Publisher():
 	data.name.append("base_link__link2")
 	data.name.append("link2__dxel2")
 
-
-	# Inverse Kinematics equations
-	# Using the derivation for the straight leg orientation i.e acute angles
-
-	l1=2.5
-	l2=2.5
-	x=3
-	y=3
-	theta2=math.acos((x*x+y*y-(l1*l1+l2*l2))/(2*l1*l2))
-	k1=l1+l2*math.cos(theta2)
-	k2=l2*math.sin(theta2)
-	theta1=math.atan(y/x)-math.atan(k2/k1)
-
-	# For an elliptical gait
-	
-	'''for i in range(90):
-		data.position.append(i/2)
-		data.position.append(i)'''
-	data.position.append(theta1)
-	data.position.append(theta2)
-
-
 	while not rospy.is_shutdown():
-		joint_pub.publish(data)
-		my_rate.sleep()
+        # Inverse Kinematics equations
+	    # Using the derivation for the straight leg orientation i.e acute angles
+		l1=2.5
+		l2=2.5
+		for x, y in zip(range(1, 3), range(1, 3)):
+			theta2=math.acos((x*x+y*y-(l1*l1+l2*l2))/(2*l1*l2))
+			k1=l1+l2*math.cos(theta2)
+			k2=l2*math.sin(theta2)
+			theta1=math.atan(y/x)-math.atan(k2/k1)
+			data.position.append(theta1)
+			data.position.append(theta2)
+			joint_pub.publish(data)
+			my_rate.sleep()
+			data.position = []
 
 if __name__=="__main__":
 	try:
